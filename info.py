e8abe075fdb76a613085b9e9c670e5c7b3cb316b
Mongodb-ai <mongodb703@gmail.com>
2026-01-06 09:09:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,8 +28,8 @@ USERNAME = environ.get('USERNAME', "@Princeny4") # ADMIN USERNAME
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002249653012').split()]
 
 # ForceSub Channel & Log Channels
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1001728907275'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1001728907275'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', -1001728907275))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', -1001728907275))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "0"))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0')) 
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
